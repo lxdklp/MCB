9117afd85954034fddec5447d5d9c46e282dadfa
lxdklp <lxdklp@lxdklp.top>
2025-09-14 02:47:33 +0800

    增加关于界面

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mcb/setting/theme.dart'; // 引入主题设置页面
+import 'package:mcb/setting/theme.dart';
+import 'package:mcb/setting/about.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -13,18 +14,31 @@ class SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('APP设置'),
       ),
       body: ListView(
         children: [
           Card(
             child: ListTile(
               leading: const Icon(Icons.color_lens),
-              title: const Text('主题设置'),
+              title: const Text('\n主题设置\n'),
               trailing: const Icon(Icons.chevron_right),
               onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const ThemePage()),
+              );
+            },
+          ),
+          ),Card(
+            child: ListTile(
+              leading: Icon(Icons.info),
+              title: const Text('\n关于\n'),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AboutPage()),
               );
             },
           ),
